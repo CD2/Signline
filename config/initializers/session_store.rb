66a5af37f44481a199a5536d66d386295d5cb8c4
Shane <shane@cd2solutions.co.uk>
2015-11-15 12:21:51 +0000

    retail ready

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_new_store', domain: :all, :tld_length => 2
+Rails.application.config.session_store :cookie_store, key: '_new_store', domain: '.signsandprinting.co.uk', :tld_length => 2
